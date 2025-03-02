90801c0fd29c9e4e9ff352092880b408bb24ee8a
Rida jahan <140480103+rida2911@users.noreply.github.com>
2025-03-02 21:06:53 +0530

    Create 40.Linked List Cycle.java

---
+++ 40.Linked List Cycle.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head ;
+        ListNode fast = head ;
+
+        while( fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next ;
+            if(slow == fast) 
+            return true;
+        }
+
+        return false  ;
+        
+    }
+
+    
+} 
+
+// TC = 0(N)
+// SC = 0(1)
