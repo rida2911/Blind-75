cb2a45be0db5271cb7069608cd9a097b48b5a8ac
Rida jahan <140480103+rida2911@users.noreply.github.com>
2025-03-18 01:36:32 +0530

    Create  Code Testcase Test64. Serialize and Deserialize Binary Tree.java

---
+++ Code Testcase Test64. Serialize and Deserialize Binary Tree.java
@@ -0,0 +1,34 @@
+
+class Codec {
+    // Serialize (Tree -> String)
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        serializeHelper(root, sb);
+        return sb.toString();
+    }
+
+    private void serializeHelper(TreeNode root, StringBuilder sb) {
+        if (root == null) {
+            sb.append("null,");
+            return;
+        }
+        sb.append(root.val).append(",");
+        serializeHelper(root.left, sb);
+        serializeHelper(root.right, sb);
+    }
+
+    // Deserialize (String -> Tree)
+    public TreeNode deserialize(String data) {
+        Queue<String> nodes = new LinkedList<>(Arrays.asList(data.split(",")));
+        return deserializeHelper(nodes);
+    }
+
+    private TreeNode deserializeHelper(Queue<String> nodes) {
+        String val = nodes.poll();
+        if (val.equals("null")) return null;
+        TreeNode root = new TreeNode(Integer.parseInt(val));
+        root.left = deserializeHelper(nodes);
+        root.right = deserializeHelper(nodes);
+        return root;
+    }
+}
